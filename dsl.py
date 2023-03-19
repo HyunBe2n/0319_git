96cf0a1f0be82f575b7a666d9f57f1eed6810843
HyunBeen Jang <kori834@yonsei.ac.kr>
2023-03-19 15:40:16 +0900

    add Sunmin

--- dsl.py
+++ dsl.py
@@ -4,7 +4,9 @@ class dsl(object):
     def whois(self):
         if self.name == "HyunBeen":
             print(f'{self.name} is a 석박통합')
-        elif self.name == '':
+        elif self.name == 'Serin':
             print(f'{self.name}is a 석사과정')
+        elif self.name == 'Sunmin':
+            print(f'{self.name}is a 인턴중')
         else:
             print(f'{self.name}가 자유다')
